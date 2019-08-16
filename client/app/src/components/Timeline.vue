cca9cb6f50cdd278f0a0c400647cd9038bcc411b
Genne Chung <krista2811@naver.com>
2019-08-16 17:03:01 +0900

    [install] vuex, vuetify installed

---
+++ client/app/src/components/Timeline.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-combobox
+      v-model="keys"
+      :items="items"
+      label="Press enter to get your keywords"
+      multiple>
+    </v-combobox>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        select: ['Vuetify', 'Programming'],
+        items: [
+          'Programming',
+          'Design',
+          'Vue',
+          'Vuetify',
+        ],
+      }
+    },
+  }
+</script>
